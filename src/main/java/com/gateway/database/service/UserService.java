3d691258d85c3af90e6fb7113b0830263eb121fb
Reza Fadilah <31206753+rezafd23@users.noreply.github.com>
2020-12-24 01:14:44 +0700

    update code

--- src/main/java/com/gateway/database/service/UserService.java
+++ src/main/java/com/gateway/database/service/UserService.java
@@ -20,42 +20,55 @@ public class UserService {
     public UserService() {
     }
 
-    public String getDataUser(String params){
+    public String getDataUser(String params) {
         try {
             JSONParser parser = new JSONParser();
             JSONObject json = (JSONObject) parser.parse(params);
             json.remove("queueName");
 
-            HttpHeaders header = new HttpHeaders();
-            header.set("authorization", String.valueOf(json.get("apikey")));
-
             restTemplate = new RestTemplate();
 
+            HttpHeaders header = new HttpHeaders();
+            header.set("authorization", String.valueOf(json.get("apikey")));
             HttpEntity entity = new HttpEntity(header);
             ResponseEntity<String> response = restTemplate.exchange(
                     "http://192.168.18.5:3000/app/api/user/getUser", HttpMethod.GET, entity, String.class);
 
-            System.out.println("isiRESULT: "+response.getBody());
+            System.out.println("isiRESULT: " + response.getBody());
+            if (response.getBody().contains("401")){
+                return response.getBody();
+            }
             JSONObject responseObject = (JSONObject) parser.parse(response.getBody());
             JSONObject payload = (JSONObject) responseObject.get("payload");
-            System.out.println("APP_STATUS: "+payload.get("application_status"));
+            System.out.println("APP_STATUS: " + payload.get("application_status"));
 
-            if (payload.get("application_status").equals("APPLICATION_SUCCESS")){
+            if (payload.get("application_status").equals("APPLICATION_SUCCESS")) {
                 JSONObject personalData = (JSONObject) payload.get("personal");
 
+                HttpHeaders header2 = new HttpHeaders();
+                header.set("apikey", "1001");
+                HttpEntity entity2 = new HttpEntity(header);
+                ResponseEntity<String> response2 = restTemplate.exchange(
+                        "http://192.168.18.5:8081/api/nasabah/info/" + personalData.get("no_ktp"), HttpMethod.GET, entity2, String.class);
+                JSONObject jsonResponse2 = (JSONObject)parser.parse(response2.getBody());
+                JSONObject jsonResponse3 = (JSONObject) jsonResponse2.get("payload");
+                payload.put("banking",jsonResponse3);
+                responseObject.remove("payload");
+                responseObject.put("payload",payload);
+                return responseObject.toJSONString();
+//                return String.valueOf(response2.getBody());
+            } else {
+                return String.valueOf(response.getBody());
             }
 
-
-            return String.valueOf(response.getBody());
-
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error LOGIN PIN");
             e.printStackTrace();
             return "0";
         }
     }
 
-    public String addPersonalData(String params){
+    public String addPersonalData(String params) {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
@@ -69,20 +82,19 @@ public class UserService {
             map.put("authorization", json.get("apikey"));
             headers.setAll(map);
             json.remove("apikey");
-            System.out.println("ISIJSON: "+json);
-            if (json.get("income")!=null){
+            System.out.println("ISIJSON: " + json);
+            if (json.get("income") != null) {
                 json.remove("income");
             }
 
             HttpEntity<?> request = new HttpEntity<>(json, headers);
 
-//            System.out.println("isi URL: "+url);
             restTemplate = new RestTemplate();
             ResponseEntity<?> response = restTemplate.postForEntity("http://192.168.18.5:3000/app/api/user/addPersonalData", request, String.class);
-            System.out.println("isiRESULT: "+response.getBody());
+            System.out.println("isiRESULT: " + response.getBody());
             return String.valueOf(response.getBody());
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error Add Personal Data");
             e.printStackTrace();
             return "0";
@@ -90,7 +102,7 @@ public class UserService {
 
     }
 
-    public String addRelative(String params){
+    public String addRelative(String params) {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
@@ -103,21 +115,21 @@ public class UserService {
             map.put("authorization", json.get("apikey"));
             headers.setAll(map);
             json.remove("apikey");
-            if (json.get("income")!=null){
+            if (json.get("income") != null) {
                 json.remove("income");
             }
 
-            System.out.println("ISIJSON: "+json);
+            System.out.println("ISIJSON: " + json);
 
             HttpEntity<?> request = new HttpEntity<>(json, headers);
 
 //            System.out.println("isi URL: "+url);
             restTemplate = new RestTemplate();
             ResponseEntity<?> response = restTemplate.postForEntity("http://192.168.18.5:3000/app/api/user/addRelativeData", request, String.class);
-            System.out.println("isiRESULT: "+response.getBody());
+            System.out.println("isiRESULT: " + response.getBody());
             return String.valueOf(response.getBody());
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error Add Relative Data");
             e.printStackTrace();
             return "0";
@@ -125,7 +137,7 @@ public class UserService {
 
     }
 
-    public String addWorkData(String params){
+    public String addWorkData(String params) {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
@@ -139,17 +151,17 @@ public class UserService {
             map.put("authorization", json.get("apikey"));
             headers.setAll(map);
             json.remove("apikey");
-            System.out.println("ISIJSON: "+json);
+            System.out.println("ISIJSON: " + json);
 
             HttpEntity<?> request = new HttpEntity<>(json, headers);
 
 //            System.out.println("isi URL: "+url);
             restTemplate = new RestTemplate();
             ResponseEntity<?> response = restTemplate.postForEntity("http://192.168.18.5:3000/app/api/user/addWorkData", request, String.class);
-            System.out.println("isiRESULT: "+response.getBody());
+            System.out.println("isiRESULT: " + response.getBody());
             return String.valueOf(response.getBody());
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error Add Work");
             e.printStackTrace();
             return "0";
@@ -157,7 +169,7 @@ public class UserService {
 
     }
 
-    public String uploadEKTP(String params){
+    public String uploadEKTP(String params) {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
@@ -171,17 +183,17 @@ public class UserService {
             map.put("authorization", json.get("apikey"));
             headers.setAll(map);
             json.remove("apikey");
-            System.out.println("ISIJSON: "+json);
+            System.out.println("ISIJSON: " + json);
 
             HttpEntity<?> request = new HttpEntity<>(json, headers);
 
 //            System.out.println("isi URL: "+url);
             restTemplate = new RestTemplate();
             ResponseEntity<?> response = restTemplate.postForEntity("http://192.168.18.5:3000/app/api/user/uploadEktp", request, String.class);
-            System.out.println("isiRESULT: "+response.getBody());
+            System.out.println("isiRESULT: " + response.getBody());
             return String.valueOf(response.getBody());
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error Upload EKTP");
             e.printStackTrace();
             return "0";
@@ -189,7 +201,7 @@ public class UserService {
 
     }
 
-    public String finishRegister(String params){
+    public String finishRegister(String params) {
         try {
             MultiValueMap<String, String> headers = new LinkedMultiValueMap<>();
 
@@ -201,14 +213,14 @@ public class UserService {
             map.put("Content-Type", "application/json");
             map.put("apikey", "1001");
             headers.setAll(map);
-            System.out.println("ISIJSON: "+json);
+            System.out.println("ISIJSON: " + json);
 
             restTemplate = new RestTemplate();
 
             HttpHeaders header = new HttpHeaders();
             header.set("authorization", String.valueOf(json.get("apikey")));
             json.remove("apikey");
-            System.out.println("isi json skrg: "+json);
+            System.out.println("isi json skrg: " + json);
 
             HttpEntity<?> request = new HttpEntity<>(json, headers);
 
@@ -216,20 +228,20 @@ public class UserService {
             ResponseEntity<String> response = restTemplate.exchange(
                     "http://192.168.18.5:3000/app/api/user/finishRegis", HttpMethod.PUT, entity, String.class);
 
-            if (response.getBody().contains("Success")){
+            if (response.getBody().contains("Success")) {
                 ResponseEntity<?> response2 = restTemplate.
                         postForEntity("http://192.168.18.5:8081/api/nasabah/add/", request, String.class);
-                if (response2.getBody().toString().contains("200")){
+                if (response2.getBody().toString().contains("200")) {
 
-                return String.valueOf(response.getBody());
+                    return String.valueOf(response.getBody());
                 }
                 return "0";
-            }else {
+            } else {
                 return "0";
             }
 //            return String.valueOf(response.getBody());
 
-        } catch (Exception e){
+        } catch (Exception e) {
             System.out.println("error Finish Registration");
             e.printStackTrace();
             return "0";
